060de2671ece2272f2ad8e4c93955d482bf42828
phil-nash-sonarsource <84976999+phil-nash-sonarsource@users.noreply.github.com>
2023-07-05 17:24:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 void use_wrapper();
 void use_tokens();
 
+#if UINTPTR_MAX == 0xffFFffFF
+// 32-bit platform
+#warning Building for 32 bit
+#elif UINTPTR_MAX == 0xffFFffFFffFFffFF
+// 64-bit platform
+#warning Building for 64 bit
+#else
+#error Unknown platform - does not look either like 32-bit or 64-bit
+#endif
+
 int main() {
     use_wrapper();
     use_tokens();
